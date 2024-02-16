bf4e899531e82116d2f5558fe0a5e8ceb510274f
Mebin98 <121173175+Mebin98@users.noreply.github.com>
2024-02-16 17:52:01 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze II/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int num; string s;
+		cin >> num >> s;
+		for (int j = 0; j < s.size(); j++) {
+			for (int k = 0; k < num; k++) {
+				cout << s[j];
+			}
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
